143a8a89117e948534070b8e4d32ea4b99bddb39
bossver <1178297577@qq.com>
2019-07-11 18:22:04 +0800

    webpack相关文件

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //打包入口从哪个文件开始打包
+    entry: './src/main.js',
+    //制定打包文件的目录
+    output: {
+        path: __dirname + '/dist/js',
+        filename: 'bundle.js'
+    }
+
+}
